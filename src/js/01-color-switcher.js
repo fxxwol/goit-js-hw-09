500ae1d3bae84da5aea75530958e9e29188becfa
fxxwol <protsivroksolana2004@gmail.com>
2023-05-03 20:13:03 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+}
 
+const DELAY = 1000
+let intervalId = null
+
+refs.startBtn.addEventListener('click', onStartBtnClick)
+refs.stopBtn.addEventListener('click', onStopBtnClick)
+refs.stopBtn.setAttribute('disabled', '')
+
+function onStartBtnClick() { 
+    refs.startBtn.setAttribute('disabled', '')
+    refs.stopBtn.removeAttribute('disabled')
+    intervalId = setInterval(function changeColor() {
+        document.body.style.background = getRandomHexColor();
+        return changeColor;
+    }(), DELAY);
+}
+
+function onStopBtnClick() { 
+    refs.startBtn.removeAttribute('disabled')
+    refs.stopBtn.setAttribute('disabled', '')
+    clearInterval(intervalId)
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
